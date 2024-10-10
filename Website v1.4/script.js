3761ee5d3ea8270881b980f0c40a83b23aeaba1c
Kyle Hollands <kylehollands@gmail.com>
2024-10-09 22:05:42 -0400

    Added new website versions, and an image.

---
+++ Website v1.4/script.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const images = document.querySelectorAll('.expandable-image');
+    const overlay = document.createElement('div');
+    const body = document.body;
+    const toggleButton = document.getElementById('toggleButton');
+    const toggleImage = document.getElementById('toggleImage');
+    const downloadButton = document.getElementById('downloadButton');
+
+    overlay.classList.add('overlay');
+    body.appendChild(overlay);
+
+    const toggleExpansion = (image) => {
+        const isExpanded = image.classList.toggle('expanded');
+        overlay.classList.toggle('show-overlay');
+        body.style.overflow = isExpanded ? 'hidden' : '';
+    };
+
+    images.forEach(image => {
+        image.addEventListener('click', () => toggleExpansion(image));
+    });
+
+    overlay.addEventListener('click', () => {
+        clearTimeout(overlayClickTimeout);
+        overlayClickTimeout = setTimeout(() => {
+            images.forEach(image => image.classList.remove('expanded'));
+            overlay.classList.remove('show-overlay');
+            body.style.overflow = '';
+        }, 100);
+    });
+
+    toggleButton.addEventListener('click', () => {
+        const isFaresView = toggleImage.src.includes('scheduleV7.png');
+        toggleImage.src = isFaresView ? 'images/faresV7.png' : 'images/scheduleV7.png';
+        toggleButton.textContent = isFaresView ? 'Show Schedule' : 'Show Fares';
+    });
+
+    downloadButton.addEventListener('click', () => {
+        window.open(toggleImage.src, '_blank');
+    });
+});
